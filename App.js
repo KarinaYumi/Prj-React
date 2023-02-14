59510ea185fffdf648fb2644cae760a1eebf7c88
unknown <you@example.com>
2023-02-14 10:38:38 -0300

    Adição text e botão

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Adicionando um novo texto</Text>
+      <Text>Adicionando um cabeçalho</Text>
+      <Button title="Enviar"></Button>
       <StatusBar style="auto" />
     </View>
   );
